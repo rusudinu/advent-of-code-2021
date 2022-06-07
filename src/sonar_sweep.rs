1863f70449c94bb929029ada3ddae7631c38f502
Dinu Rusu <dinu@codingshadows.com>
2022-06-07 16:16:16 +0300

    finished sonar sweep part 2

--- src/sonar_sweep.rs
+++ src/sonar_sweep.rs
@@ -5,11 +5,33 @@ pub(crate) fn sonar_sweep() {
     let mut input: Vec<i32> = Vec::new();
 
     read_input(&mut input);
+    //mimic_input(&mut input);
 
-    println!("{}", parse_input(input));
+
+    println!("{}", parse_input_part2(input));
 }
 
-fn parse_input(input: Vec<i32>) -> i32{
+fn parse_input_part2(input: Vec<i32>) -> i32 {
+    let mut count: i32 = 0;
+    let measurement_range = 3;
+    for i in 0..input.len() - measurement_range {
+        let mut a_sum: i32 = 0;
+        let mut b_sum: i32 = 0;
+        for j in 0..measurement_range {
+            a_sum += input[i + j];
+            b_sum += input[i + j + 1];
+        }
+        if a_sum < b_sum {
+            count += 1;
+            println!("{} {} {} (increased)", input[i], a_sum, b_sum);
+        } else {
+            println!("{} {} {} (decreased)", input[i], a_sum, b_sum);
+        }
+    }
+    count
+}
+
+fn parse_input(input: Vec<i32>) -> i32 {
     let mut count: i32 = 0;
     for i in 1..input.len() {
         if input[i - 1] < input[i] {
@@ -17,6 +39,19 @@ fn parse_input(input: Vec<i32>) -> i32{
         }
     }
     count
+}
+
+fn mimic_input(input: &mut Vec<i32>) {
+    input.push(199);
+    input.push(200);
+    input.push(208);
+    input.push(210);
+    input.push(200);
+    input.push(207);
+    input.push(240);
+    input.push(269);
+    input.push(260);
+    input.push(263);
 }
 
 fn read_input(input: &mut Vec<i32>) {
